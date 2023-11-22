2316b9a098f46fa92e8198620eb112cd2347db4b
João Victor Martins <joaovmf4@gmail.com>
2023-11-22 17:49:14 -0300

    exibição das tarefas na página home concluída

--- index.js
+++ index.js
@@ -52,10 +52,8 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        
+        resposta.render('home', { tarefas })
     })
-
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
